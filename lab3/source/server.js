489206104244f05860550ac474fd8f91e45bee76
Wiktor Sztuk <48708946+sztuku@users.noreply.github.com>
2023-04-03 15:20:39 +0200

    Add files via upload

---
+++ lab3/source/server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+
+const app = express()
+
+
+
+
+function audioCancel(){
+  var url = window.location.href
+  var search_params = url.searchParams;
+  search_params.set('audioFile', 'cancel.mp3');
+}
+
+
+app.get('/', (req, res,next) => {
+
+
+   res.set("Content-Type", "text/html");
+   res.write("<style>table, td, tr {  border: 1px solid black;}</style>");
+
+    
+    res.write("Witam w aplikacji Hello World Player<br>");
+  if(req.query.videoFile){
+	res.write("<video width='320' height='240' id='videoPlayer' controls><source src='" + req.query.videoFile + "' type='video/mp4'></video><br>");
+	res.write("<script>function videoCancel(){const elem = document.getElementById('videoPlayer');elem.setAttribute('src', 'cancel.mp4');}</script>");
+	res.write("<button type='button' onclick='videoCancel()' id='videoCancel'>Cancel Video</button><br>");
+
+  res.write("<button type='button' onclick='addElement(this)' id='videoAdd' src='" + req.query.videoFile +"'>Add Video</button><br>");
+
+
+  }
+  
+  if(req.query.audioFile){
+	res.write("<audio controls id='audioPlayer'><source='" + req.query.audio +"'/></audio><br>");
+	res.write("<script>function audioCancel(){const elem = document.getElementById('audioPlayer');elem.setAttribute('src', 'cancel.mp3');}</script>");
+	res.write("<button type='button' onclick='audioCancel()' id='audioCancel'>Cancel Audio</button><br>");
+
+  res.write("<button type='button' onclick='addElement(this)' id='audioAdd' src='" + req.query.audio +"'>Add Audio</button><br>");
+
+  }
+  
+ if(req.query.imgFile){
+	res.write(" <img src='" + req.query.imgFile +"' id='posterImage'><br>");
+
+  res.write("<button type='button' onclick='addElement(this)' src='" + req.query.imgFile +"' id='imgAdd'>Add Image</button><br>");
+
+  }
+  
+
+  res.write("<table id='playlist_table'><tr><th>No.</td><th>URL</td><th>Type</td></tr></table><br></br>");
+
+  res.write("<script>function addElement(el) {  var table = document.getElementById('playlist_table');  var row = table.insertRow();  var cell1 = row.insertCell(0);  var cell2 = row.insertCell(1);  var cell3 = row.insertCell(2);  cell1.innerHTML = table.rows.length-1;  cell2.innerHTML = el.getAttribute('src');if(el.getAttribute('id')=='videoAdd'){cell3.innerHTML = 'Video';}if(el.getAttribute('id')=='audioAdd'){cell3.innerHTML = 'Audio';}if(el.getAttribute('id')=='imgAdd'){ cell3.innerHTML = 'Image';}}</script>")
+  
+ 
+  
+  res.end();
+  
+})
+
+
+
+app.listen(4080)
